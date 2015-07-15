8c2d3578748c766a0d9eb4ebcce8a07d5807a561
Theodor Stana <theodor.stana@gmail.com>
2015-07-15 15:38:50 +0200

    Implemented new sync. logic, still to be tested

--- top/closy3_cbm/closy3_cbm.vhd
+++ top/closy3_cbm/closy3_cbm.vhd
@@ -37,8 +37,14 @@ use ieee.numeric_std.all;
 entity closy3_cbm is
   generic
   (
-    -- Number of bits for the phase -- might help fit your design into the CPLD
-    g_phase_num_bits : natural := 4
+    -- Use clk1_i for synchronization mechanism; else, use clk2_i
+    g_use_clk1 : boolean := true;
+    
+    -- clk_for_sync cycles between syncclk_i cycles
+    g_cycles_to_syncclk : natural := 1000;
+    
+    -- clk_for_sync cycles to epoch
+    g_cycles_to_epoch   : natural := 4096
   );
   port
   (
@@ -66,10 +72,10 @@ entity closy3_cbm is
     
     clkdbg1_o     : out std_logic;
     clkdbg2_o     : out std_logic;
-    synced_o      : out std_logic;
-    synced_led_o  : out std_logic;
+    synced_p_o    : out std_logic;
+    synced_led_o  : out std_logic
     
-    phasediff_o   : out std_logic_vector(g_phase_num_bits-1 downto 0)
+  --  phasediff_o   : out std_logic_vector(g_phase_num_bits-1 downto 0)
   );
 end entity closy3_cbm;
 
@@ -89,188 +95,118 @@ architecture behav of closy3_cbm is
     end loop;
     return(63);
   end function f_log2_size;
-
+  
   --===========================================================================
-  -- Constants
+  -- Types
   --===========================================================================
-  -- Number of cycles between rising edges
-  constant c_count1_top       : natural := 8;
-  constant c_count2_top       : natural := 5;
-  
-  -- Use clk1 or clk2 for the epoch counter
-  -- ** use_clk1 = false implies use_clk2
-  constant c_epoch_use_clk1   : boolean := false;
+  type t_state is (
+    CHECK_N_SYNC,
+    DEASSERT_SYNC,
+    COUNT,
+    CHECK_COUNTER
+  );
 
-  -- Number of clk1/2 cycles until epoch
-  constant c_count_epoch_top  : natural := 4096;
-  
-  -- Use clk1 or clk2 for latching the PHASEDIFF and SYNCED signals
-  -- ** use_clk1 = false implies use_clk2
-  constant c_synced_use_clk1  : boolean := true;
-  
   --===========================================================================
   -- Signals
   --===========================================================================
-  -- Clock phase counters
-  signal count1       : unsigned(f_log2_size(c_count1_top)-1 downto 0);
-  signal count2       : unsigned(f_log2_size(c_count2_top)-1 downto 0);
-  signal phase1       : unsigned(g_phase_num_bits-1 downto 0);
-  signal phase2       : unsigned(g_phase_num_bits-1 downto 0);
-  signal phasediff    : unsigned(g_phase_num_bits-1 downto 0);
+  -- Sync. FSM state signal
+  signal state        : t_state;
+
+  -- Main clock for sync logic
+  signal clk_for_sync : std_logic;
+  
+  -- Counter
+  signal counter      : unsigned(f_log2_size(g_cycles_to_syncclk)-1 downto 0);
+  signal counter_rst  : std_logic;
+  
+  -- Synced status signal
   signal synced       : std_logic;
 
-  -- Epoch counter and pulse on epoch
-  signal count_epoch  : unsigned(f_log2_size(c_count_epoch_top)-1 downto 0);
-  signal epoch_p1     : std_logic;
+  -- Pulse on epoch
+  signal epoch_p      : std_logic;
   
-  -- Internally OR all reset sources
-  signal reset_int    : std_logic;
-
-  -- Signals to delay the phase counters after reset, to account for the
-  -- AD9516's delay after SYNC
-  signal delay1       : std_logic;
-  signal delay2       : std_logic;
-
 --=============================================================================
 -- architecture begin
 --=============================================================================
 begin
 
   --===========================================================================
-  -- AD9516 synchronization logic
+  -- Synchronization FSM
   --===========================================================================
-  p_ad9516_sync : process (syncclk_i)
+  p_fsm : process (syncclk_i, sync_n_i)
   begin
-    if rising_edge(syncclk_i) then
-      sync_n_o <= sync_n_i;
+    if (sync_n_i = '0') then
+      state  <= CHECK_N_SYNC;
+      synced <= '0';
+    elsif rising_edge(syncclk_i) then
+      case state is
+        when CHECK_N_SYNC =>
+          if (synced = '1') then
+            sync_n_o <= '1';
+          else
+            sync_n_o <= '0';
+            counter_rst <= '1';
+          end if;
+          state <= DEASSERT_SYNC;
+        when DEASSERT_SYNC =>
+          sync_n_o <= '1';
+          state <= COUNT;
+        when COUNT =>
+          counter_rst <= '0';
+          state <= CHECK_COUNTER;
+        when CHECK_COUNTER =>
+          if (counter = g_cycles_to_syncclk-2) then
+            synced <= '1';
+          else
+            synced <= '0';
+          end if;
+          state <= CHECK_N_SYNC;
+        when others =>
+          synced <= '0';
+          state <= CHECK_N_SYNC;
+      end case;
     end if;
-  end process p_ad9516_sync;
+  end process p_fsm;
 
   --===========================================================================
-  -- Reset sources
+  -- Counter processes
   --===========================================================================
-  reset_int <= (not sys_rst_n_i) or (not sync_n_i) or (not reset_n_i) or
-                  (not pw_down_n_i);
-                  
-  --===========================================================================
-  -- Synchronization logic
-  --===========================================================================
-  -- Clock phase counters
-  p_count1 : process (clk1_i, reset_int)
-  begin
-    if (reset_int = '1') then
-      count1 <= (others => '0');
-      phase1 <= (others => '0');
-      delay1 <= '1';
-    elsif rising_edge(clk1_i) then
-      delay1 <= '0';
-      if (lock_det_i = '1') and (delay1 = '0') then
-        count1 <= count1 + 1;
-        if (count1 = c_count1_top-1) then
-          count1 <= (others => '0');
-          phase1 <= phase1 + 1;
-        end if;
-      end if;
-    end if;
-  end process p_count1;
-      
-  p_count2 : process (clk2_i, reset_int)
-  begin
-    if (reset_int = '1') then
-      count2 <= (others => '0');
-      phase2 <= (others => '0');
-      delay2 <= '1';
-    elsif rising_edge(clk2_i) then
-      delay2 <= '0';
-      if (lock_det_i = '1') and (delay2 = '0') then
-        count2 <= count2 + 1;
-        if (count2 = c_count2_top-1) then
-          count2 <= (others => '0');
-          phase2 <= phase2 + 1;
-        end if;
-      end if;
-    end if;
-  end process p_count2;
+gen_use_clk1 : if g_use_clk1 generate
+  clk_for_sync <= clk1_i;
+end generate gen_use_clk1;
   
-  -- Phase difference and internal synced signal generation
-gen_synced_phasediff_on_clk1 : if c_synced_use_clk1 generate
-  p_synced_phasediff : process (clk1_i)
-  begin
-    if rising_edge(clk1_i) then
-      phasediff <= phase1 - phase2;
-      synced    <= '0';
-      if (phasediff = 0) then
-        synced <= '1';
-      end if;
-    end if;
-  end process p_synced_phasediff;
-end generate gen_synced_phasediff_on_clk1;
+gen_use_clk2 : if not g_use_clk1 generate
+  clk_for_sync <= clk2_i;
+end generate gen_use_clk2;
   
-gen_synced_phasediff_on_clk2 : if not c_synced_use_clk1 generate
-  p_synced_phasediff : process (clk2_i)
+  p_counter : process (clk_for_sync, counter_rst)
   begin
-    if rising_edge(clk2_i) then
-      phasediff <= phase1 - phase2;
-      synced    <= '0';
-      if (phasediff = 0) then
-        synced <= '1';
+    if (counter_rst = '1') then
+      counter <= (others => '0');
+    elsif rising_edge(clk_for_sync) then
+      counter <= counter + 1;
+      if (counter = g_cycles_to_syncclk-1) then
+        counter <= (others => '0');
       end if;
-    end if;
-  end process p_synced_phasediff;
-end generate gen_synced_phasediff_on_clk2;
-  
-  --===========================================================================
-  -- Epoch generation
-  --===========================================================================
-gen_epoch_on_clk1 : if c_epoch_use_clk1 generate
-  p_epoch : process (clk1_i, reset_int)
-  begin
-    if (reset_int = '1') then
-      count_epoch <= (others => '0');
-      epoch_p1    <= '0';
-    elsif rising_edge(clk1_i) then
-      if (lock_det_i = '1') and (synced = '1') then
-        count_epoch <= count_epoch + 1;
-        epoch_p1    <= '0';
-        if (count_epoch = c_count_epoch_top-1) then
-          count_epoch <= count_epoch + 1;
-          epoch_p1    <= '1';
-        end if;
-      end if;
-    end if;
-  end process p_epoch;
-end generate gen_epoch_on_clk1;
       
-gen_epoch_on_clk2 : if not c_epoch_use_clk1 generate
-  p_epoch : process (clk2_i, reset_int)
-  begin
-    if (reset_int = '1') then
-        count_epoch <= (others => '0');
-        epoch_p1    <= '0';
-    elsif rising_edge(clk2_i) then
-      if (lock_det_i = '1') and (synced = '1') then
-        count_epoch <= count_epoch + 1;
-        epoch_p1    <= '0';
-        if (count_epoch = c_count_epoch_top-1) then
-          count_epoch <= count_epoch + 1;
-          epoch_p1    <= '1';
-        end if;
-      end if;
+      -- epoch_p <= '0';
+      -- if (synced = '1') and (counter = g_cycles_to_epoch-1) then
+        -- epoch_p <= '1';
+      -- end if;
     end if;
-  end process p_epoch;
-end generate gen_epoch_on_clk2;
-      
+  end process p_counter;
+
   --===========================================================================
   -- Outputs
   --===========================================================================
   -- Non-clocked
-  clkdbg1_o     <= clk1_i;
-  clkdbg2_o     <= clk2_i;
+  clkdbg1_o <= clk1_i;
+  clkdbg2_o <= clk2_i;
   
   -- Clocked, see above
-  synced_o      <= epoch_p1;
-  synced_led_o  <= synced;
-  phasediff_o   <= std_logic_vector(phasediff);
+  synced_p_o   <= epoch_p;
+  synced_led_o <= synced;
+  --phasediff_o  <= (others => '0');
 
 end architecture behav;
 --=============================================================================
